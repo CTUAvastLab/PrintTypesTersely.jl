e2933b47bf451f319191e2c601436da8ae95b62f
Matěj Račinský <matej.racinsky@avast.com>
2021-02-12 12:02:50 +0100

    added version 1.6 full print

--- src/PrintTypesTersely.jl
+++ src/PrintTypesTersely.jl
@@ -31,7 +31,8 @@ function base_show_terse(io::IO, @nospecialize(x::Type))
     return
 end
 
-function base_show_full(io::IO, @nospecialize(x::Type))
+@static if VERSION < "1.6.0-"
+function base_show_full_1_5(io::IO, @nospecialize(x::Type))
     # basically copied from the Julia sourcecode, seems it's one of most robust fixes to Pevňákoviny
     # specifically function show(io::IO, @nospecialize(x::Type))
     if x isa DataType
@@ -81,13 +82,69 @@ function base_show_full(io::IO, @nospecialize(x::Type))
     print(io, " where ")
     show(io, x.var)
 end
+end
+
+@static if VERSION >= "1.6.0-"
+function base_show_full_1_6(io::IO, @nospecialize(x::Type))
+    # basically copied from the Julia sourcecode, seems it's one of most robust fixes to Pevňákoviny
+    # specifically function show(io::IO, @nospecialize(x::Type))
+    if Base.print_without_params(x)
+        Base.show_type_name(io, Base.unwrap_unionall(x).name)
+        return
+    elseif get(io, :compact, true) && Base.show_typealias(io, x)
+        return
+    elseif x isa DataType
+        Base.show_datatype(io, x)
+        return
+    elseif x isa Union
+        if get(io, :compact, true)
+            Base.show_unionaliases(io, x)
+        else
+            print(io, "Union")
+            Base.show_delim_array(io, Base.uniontypes(x), '{', ',', '}', false)
+        end
+        return
+    end
+
+    x = x::UnionAll
+    wheres = TypeVar[]
+    let io = IOContext(io)
+        while x isa UnionAll
+            var = x.var
+            if var.name === :_ || Base.io_has_tvar_name(io, var.name, x)
+                counter = 1
+                while true
+                    newname = Symbol(var.name, counter)
+                    if !Base.io_has_tvar_name(io, newname, x)
+                        var = TypeVar(newname, var.lb, var.ub)
+                        x = x{var}
+                        break
+                    end
+                    counter += 1
+                end
+            else
+                x = x.body
+            end
+            push!(wheres, var)
+            io = IOContext(io, :unionall_env => var)
+        end
+        show(io, x)
+    end
+    Base.show_wheres(io, wheres)
+end
+end
 
 function Base.show(io::IO, @nospecialize(x::Type))
     # println("_terseprint[] in Base.show: $(_terseprint[])")
     if _terseprint[]
         return base_show_terse(io, x)
     else
-        return base_show_full(io, x)
+        # VERSION < v"1.6.0-" returns true for 1.5.* and older, but for rc and beta of 1.6 it returns true
+        @static if VERSION < v"1.6.0-"
+            return base_show_full_1_5(io, x)
+        else
+            return base_show_full_1_6(io, x)
+        end
     end
 end
 
